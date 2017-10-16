bb69e1c48dfdd6db9d0937a0fd9dff7f2ef6c120
= <gaur_piyush@outlook.com>
2017-10-16 23:00:48 +0530

    server started serving html file

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = new express();
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var port = process.env.PORT || 8080;
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, function () {
+    console.log('Server is running on port: '+port);
+});
